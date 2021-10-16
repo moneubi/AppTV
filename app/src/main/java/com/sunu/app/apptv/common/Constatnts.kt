23c4e0a9c1aedcf274b0bf0e14846ddc662c22cc
psa-lmbaye <moneu01@gmail.com>
2021-10-16 19:51:19 +0100

    OCS-02 : remove unused Constant and Color file

--- app/src/main/java/com/sunu/app/apptv/common/Constatnts.kt
+++ app/src/main/java/com/sunu/app/apptv/common/Constatnts.kt
@@ -3,9 +3,7 @@ package com.sunu.app.apptv.common
 object Constatnts {
 
     const val BASE_URL = "https://api.ocs.fr"
-    const val REF_CONTENT = "contents"
     const val REF_SEARCH = "search"
     const val REF_TITLE = "title"
-    const val REF_INTETERROGATION = "?"
     const val REF_EGALE = "="
 }
