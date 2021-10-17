c412cf4711b49e7b030a98776e477202b5c28e9d
psa-lmbaye <moneu01@gmail.com>
2021-10-17 17:28:13 +0100

    OCS-04 : Add Exoplayer implementation to listen the movie

--- app/src/main/java/com/sunu/app/apptv/presentation/ui/detail/DetailMoviefragment.kt
+++ app/src/main/java/com/sunu/app/apptv/presentation/ui/detail/DetailMoviefragment.kt
@@ -11,6 +11,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.util.Util
 import com.sunu.app.apptv.R
 import com.sunu.app.apptv.common.Constatnts
 import com.sunu.app.apptv.domain.model.ContentList
@@ -25,6 +30,14 @@ class DetailMoviefragment : Fragment() {
 
     private lateinit var detailMovieViewModel: DetailMovieViewModel
     private lateinit var movie: Movies
+    /*===========================================================*/
+    private var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition = 0L
+    private val TAG = "PlayerActivity"
+    private val playbackStateListener: Player.EventListener = playbackStateListener()
+    /*===========================================================*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +58,16 @@ class DetailMoviefragment : Fragment() {
         val title = movie.title[0]
         txt_title_movie.text = title.value
         txt_subtitle.text = movie.subtitle
+        /*====================================================*/
+        img_play.setOnClickListener {
+            img_fulcreen.visibility = View.GONE
+            img_play.visibility = View.GONE
+            video_view.visibility = View.VISIBLE
+            progress_waiting.visibility = View.VISIBLE
+            if (Util.SDK_INT >= 24) {
+                initializePlayer()
+            }
+        }
     }
 
     fun getSaisons(){
@@ -67,5 +90,84 @@ class DetailMoviefragment : Fragment() {
                 }
             }
         })
+    }
+
+    /**
+     * initialize player
+     * */
+    fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(requireContext())
+            .build()
+            .also { exoPlayer ->
+                video_view.player = exoPlayer
+                val mediaItem = MediaItem.fromUri("https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-\n" +
+                        "bf1d-4e3d-8899-f0f6155f6efa.mpd")
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentWindow, playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
+                //exoPlayer.play()
+                exoPlayer.prepare()
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        video_view.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun releasePlayer() {
+        player?.run {
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentWindowIndex
+            playWhenReady = this.playWhenReady
+            removeListener(playbackStateListener)
+            release()
+        }
+        player = null
+    }
+
+    private fun playbackStateListener() = object : Player.EventListener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            val stateString: String = when (playbackState) {
+                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE"
+                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING"
+                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY"
+                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED"
+                else -> "UNKNOWN_STATE"
+            }
+            Log.d(TAG, "changed state to $stateString")
+            if (!stateString.isNullOrEmpty()){
+                progress_waiting.visibility = View.GONE
+            }
+        }
     }
 }
